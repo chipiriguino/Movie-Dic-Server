35a2bc859a57a52241dbef865f871c04090f4f1b
jose luis <joseluisblascoortiz@hotmail.com>
2020-11-18 16:23:34 +0100

    random OK

--- routes/movies.js
+++ routes/movies.js
@@ -8,7 +8,6 @@ router.get("/", async(req, res, next) => {
     const { limit } = req.query
   try {
     let movies = await Movie.find()
-    console.log('MOVIEEEEESSSSSS', movies)
     res.status(200).json(movies)
   } catch (error) {
     console.log(error)
@@ -18,13 +17,71 @@ router.get("/", async(req, res, next) => {
 router.get("/random", async(req, res, next) => {
 
   try {
-    let movies = await Movie.find().limit(Number(limit))
-    console.log('MOVIEEEEESSSSSS', movies)
-    res.status(200).json(movies)
+    
+    let moviesrandom = await Movie.find()
+    let moviesran =Math.floor(Math.random()*moviesrandom.length)
+    res.status(200).json(moviesrandom[moviesran])
   } catch (error) {
     console.log(error)
   }
 });
+
+// ADD MOVIE
+
+// router.get("/myprofile/add-movie", withAuth, function (req, res, next) {
+//   res.render("/add-event");
+// });
+
+// router.post(
+//   "/myprofile/add-movie",
+//   uploadCloud.single("photo"),
+//   withAuth,
+//   async (req, res, next) => {
+//     const { name, description } = req.body;
+
+//   if (name.length < 5) {
+//       res.render("/myprofile/add-movie", {
+//         errorMessage: "Your event name should have at least 5 characters",
+//       });
+//       return;
+//     } else if (description.length < 5) {
+//       res.render("/myprofile/add-movie", {
+//         errorMessage: "Write a longer description!",
+//       });
+//       return;
+//     }
+//   try {
+//     const addMovie = await Event.findOne({ name: name});
+//     if (addMovie !== null) {
+//       res.render("/myprofile/add-movie", {
+//         errorMessage: "This movie already exists!",
+//       });
+//       return;
+//     }
+
+//     const imgPath = req.file.url;
+
+//     await Event.create({
+//       name,
+//       description,
+//       imgPath,
+//     });
+//     res.redirect("all-events");
+//   } catch (error) {
+//     next(error);
+//   }
+//   User.findOneAndUpdate(
+//     { _id: req.session.currentUserInfo._id },
+//     { $push: { movies: addMovie.id } },
+//     { new: true }
+//   ).then((user) => console.log("The movie was created!"));
+// }
+// );
+
+
+
+
+
 
 
 
