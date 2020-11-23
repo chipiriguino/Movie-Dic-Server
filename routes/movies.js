d25c8c095ee698dd86e283683d81dbb3a2138923
GitSkynet <carlos.curtido@gmail.com>
2020-11-23 10:14:02 +0100

    push Lunes

--- routes/movies.js
+++ routes/movies.js
@@ -50,6 +50,16 @@ const Movie = require('../models/movie');
   }
   });
 
+  router.get("/movies/carrousel", async(req, res, next) => {
+    try {
+      let movies = await Movie.find().limit(8)
+      res.status(200).json(movies)
+    } catch (error) {
+      console.log(error)
+    }
+    });
+  
+
   // TOP-RATED ROUTE
 
   router.get("/top-rated", async(req, res, next) => {
